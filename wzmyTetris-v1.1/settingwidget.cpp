5eb286d0a7ea2383cac6fa155f25d2ce0d67189f
toptaptip <86899679+toptaptip@users.noreply.github.com>
2021-07-06 10:03:13 +0800

    1.1

--- wzmyTetris-v1.1/settingwidget.cpp
+++ wzmyTetris-v1.1/settingwidget.cpp
@@ -9,12 +9,13 @@
 settingWidget::settingWidget(QWidget *parent) : QDialog(parent)
 {
     setWindowTitle("Settings");
-    this->setFixedSize(400,800);
+    this->setFixedSize(300,400);
 
     //开始按钮
-    MyButton * startBtn = new MyButton(":/res/start1.png");
+    MyButton * startBtn = new MyButton(":/Image/settingbutton2");
     startBtn->setParent(this);
-    startBtn->move(70,150);
+    startBtn->setFixedSize(200,90);
+    startBtn->move(this->width()*0.2,this->height()*0.18);
     connect(startBtn,&MyButton::clicked,[=]()
     {
        startBtn->Zoom1();
@@ -27,9 +28,10 @@ settingWidget::settingWidget(QWidget *parent) : QDialog(parent)
 
 
     //重开按钮
-    MyButton * restartBtn = new MyButton(":/res/restart .png");
+    MyButton * restartBtn = new MyButton(":/Image/settingbutton3");
     restartBtn->setParent(this);
-    restartBtn->move(70,300);
+    restartBtn->setFixedSize(200,90);
+    restartBtn->move(this->width()*0.2,this->height()*0.36);
     connect(restartBtn,&MyButton::clicked,[=]()
     {
        restartBtn->Zoom1();
@@ -43,9 +45,10 @@ settingWidget::settingWidget(QWidget *parent) : QDialog(parent)
 
 
     //音乐按钮
-    MyButton * musicBtn = new MyButton(":/res/music.png");
+    MyButton * musicBtn = new MyButton(":/Image/settingbutton4");
     musicBtn->setParent(this);
-    musicBtn->move(70,450);
+    musicBtn->setFixedSize(200,90);
+    musicBtn->move(this->width()*0.2,this->height()*0.54);
     connect(musicBtn,&MyButton::clicked,[=]()
     {
        musicBtn->Zoom1();
@@ -55,9 +58,10 @@ settingWidget::settingWidget(QWidget *parent) : QDialog(parent)
 
 
     //home按钮
-    MyButton * homeBtn = new MyButton(":/res/home.png");
+    MyButton * homeBtn = new MyButton(":/Image/settingbutton5");
     homeBtn->setParent(this);
-    homeBtn->move(70,600);
+    homeBtn->setFixedSize(200,90);
+    homeBtn->move(this->width()*0.2,this->height()*0.72);
     connect(homeBtn,&MyButton::clicked,[=]()
     {
        homeBtn->Zoom1();
@@ -74,7 +78,15 @@ void settingWidget::paintEvent (QPaintEvent *)
 {
     //绘制背景图
     QPainter painter(this);
-    QPixmap bgpix;
-    bgpix.load(":/res/settingbackground.jpg");
-    painter.drawPixmap(0,0,bgpix);
+
+    QPen pen;
+
+    painter.setPen(pen);
+
+
+    QBrush brush(QColor(90,90,90));
+
+    painter.setBrush(brush);
+
+    painter.drawRect(QRect(0,0,this->width(),this->height()));
 }
